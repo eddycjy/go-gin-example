c4cbaffcd2f83cbcc1493f22204a406807da958f
EDDYCJY <313687982@qq.com>
2018-02-28 00:01:35 +0800

    setting.go: log.Fatal -> log.Fatalf

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -24,7 +24,7 @@ func init() {
 	var err error
 	Cfg, err = ini.Load("conf/app.ini")
 	if err != nil {
-		log.Fatal(2, "Fail to parse 'conf/app.ini': %v", err)
+		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
 	}
 
 	LoadBase()
@@ -39,7 +39,7 @@ func LoadBase() {
 func LoadServer() {
 	sec, err := Cfg.GetSection("server")
 	if err != nil {
-		log.Fatal(2, "Fail to get section 'server': %v", err)
+		log.Fatalf("Fail to get section 'server': %v", err)
 	}
 
 	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
@@ -52,7 +52,7 @@ func LoadServer() {
 func LoadApp() {
 	sec, err := Cfg.GetSection("app")
 	if err != nil {
-		log.Fatal(2, "Fail to get section 'app': %v", err)
+		log.Fatalf("Fail to get section 'app': %v", err)
 	}
 
 	JwtSecret = sec.Key("JWT_SECRET").MustString("!@)*#)!@U#@*!@!)")
