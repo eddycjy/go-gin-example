62981d86a5066f3b8c76eee124575af0f7048b59
EDDYCJY <313687982@qq.com>
2018-05-27 22:38:35 +0800

    修改文章表，增加封面图地址字段

--- docs/sql/blog.sql
+++ docs/sql/blog.sql
@@ -22,6 +22,7 @@ CREATE TABLE `blog_article` (
   `title` varchar(100) DEFAULT '' COMMENT '文章标题',
   `desc` varchar(255) DEFAULT '' COMMENT '简述',
   `content` text COMMENT '内容',
+  `cover_image_url` varchar(255) DEFAULT '' COMMENT '封面图片地址',
   `created_on` int(10) unsigned DEFAULT '0' COMMENT '新建时间',
   `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
   `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
