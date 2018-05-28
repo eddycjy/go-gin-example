8c258bacbb4c4b58bef2e787e5ef93ede7c85cb1
EDDYCJY <313687982@qq.com>
2018-05-28 14:13:07 +0800

    fix AddTag validation error

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -64,9 +64,9 @@ func AddTag(c *gin.Context) {
 
 	valid := validation.Validation{}
 	valid.Required(name, "name").Message("名称不能为空")
-	valid.Required(name, "created_by").Message("创建人不能为空")
-	valid.MaxSize(name, 100, "created_by").Message("创建人最长为100字符")
 	valid.MaxSize(name, 100, "name").Message("名称最长为100字符")
+	valid.Required(createdBy, "created_by").Message("创建人不能为空")
+	valid.MaxSize(createdBy, 100, "created_by").Message("创建人最长为100字符")
 	valid.Range(state, 0, 1, "state").Message("状态只允许0或1")
 
 	code := e.INVALID_PARAMS
