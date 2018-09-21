6ff68d89af0e2f2e4246d16a635d7e20b56512b7
EDDYCJY <313687982@qq.com>
2018-09-21 17:34:54 +0800

    check JWT.util.ParseToken error

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -3,8 +3,8 @@ package jwt
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 
 	"github.com/EDDYCJY/go-gin-example/pkg/e"
 	"github.com/EDDYCJY/go-gin-example/pkg/util"
@@ -21,11 +21,13 @@ func JWT() gin.HandlerFunc {
 			code = e.INVALID_PARAMS
 		} else {
 			_, err := util.ParseToken(token)
-			switch err.(*jwt.ValidationError).Errors {
-			case jwt.ValidationErrorExpired:
-				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
-			default:
-				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+			if err != nil {
+				switch err.(*jwt.ValidationError).Errors {
+				case jwt.ValidationErrorExpired:
+					code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+				default:
+					code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
+				}
 			}
 		}
 
