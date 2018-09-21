85da49952894e9deb89416b815626dc833f7ca91
EDDYCJY <313687982@qq.com>
2018-09-21 17:28:55 +0800

    fix #12 jwt.ValidationErrorExpired

--- middleware/jwt/jwt.go
+++ middleware/jwt/jwt.go
@@ -2,9 +2,9 @@ package jwt
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/dgrijalva/jwt-go"
 
 	"github.com/EDDYCJY/go-gin-example/pkg/e"
 	"github.com/EDDYCJY/go-gin-example/pkg/util"
@@ -20,11 +20,12 @@ func JWT() gin.HandlerFunc {
 		if token == "" {
 			code = e.INVALID_PARAMS
 		} else {
-			claims, err := util.ParseToken(token)
-			if err != nil {
-				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
-			} else if time.Now().Unix() > claims.ExpiresAt {
+			_, err := util.ParseToken(token)
+			switch err.(*jwt.ValidationError).Errors {
+			case jwt.ValidationErrorExpired:
 				code = e.ERROR_AUTH_CHECK_TOKEN_TIMEOUT
+			default:
+				code = e.ERROR_AUTH_CHECK_TOKEN_FAIL
 			}
 		}
 
