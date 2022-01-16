7760a77f877fb1bedaf638c11d74ad059dea558b
nullsimon <uuapril@gmail.com>
2022-01-16 21:44:55 +0800

    修复GetArticles的获取参数方式，改为Query

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -68,13 +68,13 @@ func GetArticles(c *gin.Context) {
 	valid := validation.Validation{}
 
 	state := -1
-	if arg := c.PostForm("state"); arg != "" {
+	if arg := c.Query("state"); arg != "" {
 		state = com.StrTo(arg).MustInt()
 		valid.Range(state, 0, 1, "state")
 	}
 
 	tagId := -1
-	if arg := c.PostForm("tag_id"); arg != "" {
+	if arg := c.Query("tag_id"); arg != "" {
 		tagId = com.StrTo(arg).MustInt()
 		valid.Min(tagId, 1, "tag_id")
 	}
